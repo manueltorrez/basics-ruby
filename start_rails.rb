01edf9042f59c582963051f6c37a379541e4dc07
Manuel Torrez <mastervado98@gmail.com>
2018-01-29 14:48:42 -0600

    Using routes.rb to redirect

--- start_rails.rb
+++ start_rails.rb
@@ -5,3 +5,10 @@ $ rails s -b $IP -p $PORT
 #Create Rails project
 
 $ rails new project_name
+
+#On config/routes.rb, "pages" means the controller and "home" is the action aka method in the controller
+
+root "pages#home"
+
+#On config/routes.rb, it gets the /about request and redirects it to an specific page using the action in the "pages" controller
+get '/about', to 'pages#about'
